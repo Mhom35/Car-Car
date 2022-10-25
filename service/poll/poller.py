0e8b7a5e1be926b3fbfcf494842e7e8030f8980f
Hieu <thanhhieu.sgn@gmail.com>
2022-10-25 04:50:35 -0400

    finished all Service Appointment backends

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -12,15 +12,29 @@ django.setup()
 # Import models from service_rest, here.
 # from service_rest.models import Something
 
+from service_rest.models import AutomobileVO
+
+def get_automobiles():
+    response = requests.get('http://inventory-api:8000/api/automobiles/')
+    content = json.loads(response.content)
+    for auto in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            import_href=auto["href"],
+            defaults={
+                "vin": auto["vin"],
+            }
+        )
+
+
 def poll():
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
+            get_automobiles();
             pass
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(60)
+        time.sleep(10)
 
 
 if __name__ == "__main__":
